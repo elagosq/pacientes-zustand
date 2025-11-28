3f1b83ab9f3ce775ba9cea4ace10bea928a0bd25
Esteban Lagos <elagosquintanilla@gmail.com>
2025-11-27 23:24:20 -0300

    Primer commit

---
+++ src/store.ts
@@ -0,0 +1,62 @@
+import { create } from "zustand"
+import { devtools, persist } from "zustand/middleware"
+import { v4 as uuidv4 } from 'uuid'
+import type { DraftPatient, Patient } from "./types"
+
+type PatientState = {
+  patients: Patient[]
+  activeId: Patient['id']
+  addPatient:(data: DraftPatient) => void
+  deletePatient:(id: Patient['id']) => void
+  getPatientById:(id: Patient['id']) => void
+  updatePatient:(data: DraftPatient) => void
+}
+
+const createPatient = (patient: DraftPatient) : Patient => {
+    return {...patient, id: uuidv4()}
+}
+
+export const usePatientStore = create<PatientState>()(
+    devtools(
+      persist(
+        (set) => ({
+           patients:[],
+           activeId:'',
+           addPatient: (data) => {
+              const newPatient = createPatient(data)
+              set((state) => ({
+                 patients:[...state.patients, newPatient]
+              }),
+              undefined,
+              "patient/addPatient"
+             ) 
+           },
+           deletePatient:(id) => {
+              set((state) => ({
+               patients: state.patients.filter(patient => patient.id !== id)
+              }),
+              undefined,
+              "patient/deletePatient"            
+            )
+           },
+           getPatientById:(id) => {
+             set(() => ({
+               activeId: id
+             }),
+             undefined,
+             "patient/getPatientById"
+             )
+           },
+           updatePatient:(data) => {
+              set((state) => ({
+                 patients: state.patients.map(patient => patient.id === state.activeId ? {id: state.activeId, ...data} : patient),
+                 activeId:''
+              }),
+              undefined,
+              "patient/udpatePatient"
+            )
+           }
+        }),{
+           name:"patient-storage"
+        })
+))
---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+//type par registrar un paciente
+export type Patient = {
+   id: string
+   name: string
+   caretaker: string
+   email: string
+   date: Date
+   symptoms: string
+}
+
+export type DraftPatient = Omit<Patient,'id'>
